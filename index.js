678b1e3ac1ea4d83fc6acaa58bc7768df622abbe
Nico-Alto <78941387+nico-alto@users.noreply.github.com>
2021-06-24 21:34:22 -0300

    encolo el cambio de titulo
    cambio de titulo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get('/ejemplo', (req, res) => {
-    res.end('Llegaste ejemplo');
+    res.end('Llegaste ejemplo de Vercel');
 });
 app.get('/usuarios', (req, res) => {
     res.end('Llegaste usuarios');
